2c34c608414b67dbb06748aca241c1b03b2be766
Jaka Tasnim <136361749+JakaTasnim@users.noreply.github.com>
2026-05-17 20:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (48.41%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+         vector<int> row(rowIndex + 1, 0);
+
+        row[0] = 1;
+
+        for(int i = 1; i <= rowIndex; i++) {
+
+            // Update from right to left
+            for(int j = i; j >= 1; j--) {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+
+        return row;
+        
+    }
+};
