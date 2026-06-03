3538e2f8f5c6cc15e03989b4e5394aed1c085aec
Jaka Tasnim <136361749+JakaTasnim@users.noreply.github.com>
2026-06-03 20:40:45 +0530

    Time: 131 ms (19.27%) | Memory: 262.1 MB (19.27%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -3,9 +3,8 @@ public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int n = landStartTime.size();
         int m = waterStartTime.size();
-
-        // Water rides preprocessing
         vector<pair<int, int>> water(m);
+
         for (int i = 0; i < m; i++) {
             water[i] = {waterStartTime[i], waterDuration[i]};
         }
@@ -13,30 +12,31 @@ public:
         sort(water.begin(), water.end());
 
         vector<int> wStart(m);
-        vector<long long> prefMinDurW(m);
-        vector<long long> suffMinFinishW(m);
+        vector<int> prefMinDurW(m);
+        vector<int> suffMinFinishW(m);
 
         for (int i = 0; i < m; i++) {
+
             wStart[i] = water[i].first;
 
             if (i == 0)
                 prefMinDurW[i] = water[i].second;
             else
-                prefMinDurW[i] =
-                    min(prefMinDurW[i - 1], (long long)water[i].second);
+                prefMinDurW[i] = min(prefMinDurW[i - 1], water[i].second);
         }
 
         for (int i = m - 1; i >= 0; i--) {
-            long long val = (long long)water[i].first + water[i].second;
+
+            int finish =
+                water[i].first + water[i].second;
 
             if (i == m - 1)
-                suffMinFinishW[i] = val;
+                suffMinFinishW[i] = finish;
             else
-                suffMinFinishW[i] = min(suffMinFinishW[i + 1], val);
+                suffMinFinishW[i] = min(suffMinFinishW[i + 1], finish);
         }
-
-        // Land rides preprocessing
         vector<pair<int, int>> land(n);
+
         for (int i = 0; i < n; i++) {
             land[i] = {landStartTime[i], landDuration[i]};
         }
@@ -44,73 +44,87 @@ public:
         sort(land.begin(), land.end());
 
         vector<int> lStart(n);
-        vector<long long> prefMinDurL(n);
-        vector<long long> suffMinFinishL(n);
+        vector<int> prefMinDurL(n);
+        vector<int> suffMinFinishL(n);
 
         for (int i = 0; i < n; i++) {
+
             lStart[i] = land[i].first;
 
             if (i == 0)
                 prefMinDurL[i] = land[i].second;
             else
-                prefMinDurL[i] =
-                    min(prefMinDurL[i - 1], (long long)land[i].second);
+                prefMinDurL[i] = min(prefMinDurL[i - 1], land[i].second);
         }
 
         for (int i = n - 1; i >= 0; i--) {
-            long long val = (long long)land[i].first + land[i].second;
+
+            int finish =
+                land[i].first + land[i].second;
 
             if (i == n - 1)
-                suffMinFinishL[i] = val;
+                suffMinFinishL[i] = finish;
             else
-                suffMinFinishL[i] = min(suffMinFinishL[i + 1], val);
+                suffMinFinishL[i] = min(suffMinFinishL[i + 1], finish);
         }
 
-        long long ans = LLONG_MAX;
-
-        // Land -> Water
+        int ans = INT_MAX;
         for (int i = 0; i < n; i++) {
 
-            long long t =
-                (long long)landStartTime[i] + landDuration[i];
+            int landFinish =
+                landStartTime[i] + landDuration[i];
 
-            long long best = LLONG_MAX;
+            int pos =
+                upper_bound(wStart.begin(),
+                            wStart.end(),
+                            landFinish) -
+                wStart.begin();
 
-            int pos = upper_bound(wStart.begin(), wStart.end(), t) -
-                      wStart.begin();
+            int best = INT_MAX;
 
-            // water ride already open
+            // Water ride already open
             if (pos > 0) {
-                best = min(best, t + prefMinDurW[pos - 1]);
+
+                best =
+                    min(best,
+                        landFinish +
+                        prefMinDurW[pos - 1]);
             }
 
-            // water ride opens later
+            // Water ride opens later
             if (pos < m) {
-                best = min(best, suffMinFinishW[pos]);
+
+                best =
+                    min(best,
+                        suffMinFinishW[pos]);
             }
 
             ans = min(ans, best);
         }
-
-        // Water -> Land
         for (int i = 0; i < m; i++) {
 
-            long long t =
-                (long long)waterStartTime[i] + waterDuration[i];
+            int waterFinish =
+                waterStartTime[i] + waterDuration[i];
 
-            long long best = LLONG_MAX;
+            int pos =upper_bound(lStart.begin(),lStart.end(),waterFinish) - lStart.begin();
 
-            int pos = upper_bound(lStart.begin(), lStart.end(), t) -
-                      lStart.begin();
+            int best = INT_MAX;
 
-            // land ride already open
+            // Land ride already open
             if (pos > 0) {
-                best = min(best, t + prefMinDurL[pos - 1]);
+
+                best =
+                    min(best,
+                        waterFinish +
+                        prefMinDurL[pos - 1]);
             }
 
-            // land ride opens later
+            // Land ride opens later
             if (pos < n) {
-                best = min(best, suffMinFinishL[pos]);
+
+                best =
+                    min(best,
+                        suffMinFinishL[pos]);
             }
 
             ans = min(ans, best);
